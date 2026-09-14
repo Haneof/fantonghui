403e653ab9f5ba17ac22acd8fb207ab4f34d735c
Haneof <131862491+Haneof@users.noreply.github.com>
2026-09-14 14:30:24 +0800

    M0-021 export frozen state-machine helpers

--- src/aios_core/services/__init__.py
+++ src/aios_core/services/__init__.py
@@ -1,3 +1,17 @@
-from .state_machines import validate_event_transition, validate_task_transition
+from .state_machines import (
+    allowed_event_transitions,
+    allowed_task_transitions,
+    validate_event_revision_transition,
+    validate_event_transition,
+    validate_task_revision_transition,
+    validate_task_transition,
+)
 
-__all__ = ["validate_event_transition", "validate_task_transition"]
+__all__ = [
+    "allowed_event_transitions",
+    "allowed_task_transitions",
+    "validate_event_revision_transition",
+    "validate_event_transition",
+    "validate_task_revision_transition",
+    "validate_task_transition",
+]
